365478278abed335eb85c0558818e289b80d4a99
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-03-17 10:57:29 +0200

    Create package folders and add JUnit dependencies

---
+++ src/main/java/za/ac/cput/domain/Vehicle.java
@@ -0,0 +1,21 @@
+package za.ac.cput.domain;
+
+public class Vehicle {
+    private String make;
+    private String model;
+    private String color;
+    private int wheels;
+
+    String drive(){
+        return null;
+    }
+
+    String useBreak(){
+        return null;
+    }
+
+    String changeDirection(){
+        return null;
+    }
+
+}
